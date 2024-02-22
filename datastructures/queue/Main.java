03f76186dc4b1f0ef12c4659987caac49199210d
Nishant Gupta <hunnygupta0315@gmail.com>
2024-02-22 13:10:29 +0530

    Queue

--- datastructures/queue/Main.java
+++ datastructures/queue/Main.java
@@ -4,27 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Queue myQueue = new Queue(2);
+        Queue myQueue = new Queue(1);
+        myQueue.enqueue(2);
+        myQueue.enqueue(3);
 
-        System.out.println("Before enqueue():");
-        System.out.println("--------------");
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.getLength();
+        //Dequeue
+        // (2) Items - Returns 2 Node
+        System.out.println(myQueue.dequeue().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myQueue.dequeue().value);
+        // (0) Items - Returns null
+        System.out.println(myQueue.dequeue());
 
-        System.out.println("\nQueue:");
-        myQueue.printQueue();
+        // Enqueue
+        // System.out.println("Before enqueue():");
+        // System.out.println("--------------");
+        // myQueue.getFirst();
+        // myQueue.getLast();
+        // myQueue.getLength();
 
-        myQueue.enqueue(1);
+        // System.out.println("\nQueue:");
+        // myQueue.printQueue();
 
-        System.out.println("\n\nAfter enqueue():");
-        System.out.println("-------------");
-        myQueue.getFirst();
-        myQueue.getLast();
-        myQueue.getLength();
+        // myQueue.enqueue(1);
 
-        System.out.println("\nQueue:");
-        myQueue.printQueue();
+        // System.out.println("\n\nAfter enqueue():");
+        // System.out.println("-------------");
+        // myQueue.getFirst();
+        // myQueue.getLast();
+        // myQueue.getLength();
+
+        // System.out.println("\nQueue:");
+        // myQueue.printQueue();
 
     }
 
