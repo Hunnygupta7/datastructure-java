bccd814f4c942b4b7d23780fe3de403d6e0df532
Nishant Gupta <hunnygupta0315@gmail.com>
2024-02-29 12:24:49 +0530

    Binary Search Tree

--- datastructures/binarysearchtree/Main.java
+++ datastructures/binarysearchtree/Main.java
@@ -5,9 +5,49 @@ public class Main {
 
         BinarySearchTree myBST = new BinarySearchTree();
 
-        myBST.insert(2);
-        myBST.insert(1);
-        myBST.insert(3);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+
+        System.out.println("BST Contains 27:");
+        System.out.println(myBST.contains(27));
+
+        System.out.println("\nBST Contains 17:");
+        System.out.println(myBST.contains(17));
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            BST Contains 27:
+            true
+            
+            BST Contains 17:
+            false
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        //BST-INSERT
+        // myBST.insert(2);
+        // myBST.insert(1);
+        // myBST.insert(3);
 
         /*
          * THE LINES ABOVE CREATE THIS TREE:
@@ -16,9 +56,9 @@ public class Main {
          * 1 3
          */
         // ROOT MUST BE PUBLIC FOR THESE LINES TO WORK
-        System.out.println("Root: " + myBST.root.value);
-        System.out.println("\nRoot->Left: " + myBST.root.left.value);
-        System.out.println("\nRoot->Right: " + myBST.root.right.value);
+        // System.out.println("Root: " + myBST.root.value);
+        // System.out.println("\nRoot->Left: " + myBST.root.left.value);
+        // System.out.println("\nRoot->Right: " + myBST.root.right.value);
 
         /*
          * EXPECTED OUTPUT:
